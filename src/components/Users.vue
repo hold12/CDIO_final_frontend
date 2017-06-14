f9680cf893d3bf29f6d541db7066fa80e05ee589
twast92 <twast.92@gmail.com>
2017-06-14 22:24:24 +0200

    Did stuff

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import auth from '../auth'
 export default {
   name: 'users',
   data () {
@@ -29,7 +30,7 @@ export default {
           'Accept': 'application/json'
       }, {
         headers: {
-          'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJob2xkMTIiLCJ1c2VyIjp7InVzZXJJZCI6MSwiZmlyc3RuYW1lIjoiYWRtaW4iLCJsYXN0bmFtZSI6bnVsbCwiaW5pdGlhbHMiOiJhZG0iLCJwYXNzd29yZCI6IltoaWRkZW5dIiwiYWN0aXZlIjp0cnVlLCJyb2xlcyI6W3sicm9sZV9pZCI6MCwicm9sZV9uYW1lIjoiYWRtaW5pc3RyYXRvciIsInBlcm1pc3Npb25zIjpbInVzZXIuY3JlYXRlIiwidXNlci51cGRhdGUiLCJ1c2VyLmRlbGV0ZSIsInVzZXIucmVhZCJdfSx7InJvbGVfaWQiOjAsInJvbGVfbmFtZSI6ImZvcmVtYW4iLCJwZXJtaXNzaW9ucyI6WyJpbmdyZWRpZW50YmF0Y2guY3JlYXRlIiwiaW5ncmVkaWVudGJhdGNoLnJlYWQiLCJwcm9kdWN0YmF0Y2guY3JlYXRlIiwicHJvZHVjdGJhdGNoLnJlYWQiXX0seyJyb2xlX2lkIjowLCJyb2xlX25hbWUiOiJwaGFybWFjaXN0IiwicGVybWlzc2lvbnMiOlsiaW5ncmVkaWVudC5jcmVhdGUiLCJpbmdyZWRpZW50LnJlYWQiLCJyZWNpcGUuY3JlYXRlIiwicmVjaXBlLnJlYWQiXX1dfSwiZXhwIjoxNDk3NDU1NjQ1fQ.9zfRoFvNTAIUP3kZRRiyG_2ORxeQEo7qkfgl16lSKrv3ml5PB9k1dSXAmvuLwnAe_CX-FRCMDnB0-E6ZzHOIXA',
+          'Authorization': auth.getAuthHeader()
         }
       }).then((response) => {
         users = JSON.parse(response.data)
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
+import App from './components/App.vue'
+import Login from './components/Login.vue'
+import Users from './components/Users.vue'
 import vueResource from 'vue-resource'
+import auth from './auth'
 
 Vue.config.productionTip = false
 Vue.use(vueResource)
+
+auth.checkAuth()
+
+Vue.http.headers.common['Authorization'] = auth.getAuthHeader();
 
 /* eslint-disable no-new */
 new Vue({
